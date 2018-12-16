11ff8b85126aa408ba30c39157639c348cbbd76d
MichaelGreisberger <mike.greisberger@gmail.com>
2018-12-16 09:47:54 +0100

    kleine Aenderung

--- src/app/components/DataTable/DataTable/index.tsx
+++ src/app/components/DataTable/DataTable/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { inject } from 'mbox-react'
-import { STORE_ROUTER } from 'app/constants'
+// import { inject } from 'mbox-react'
+// import { STORE_ROUTER } from 'app/constants'
 
-@inject(STORE_ROUTER)
+// @inject(STORE_ROUTER)
 
 export default class DataTable extends React.Component {
   render() {
     return <div>TableEntries here</div>
+
   }
 }
--- src/app/components/DataTable/TableEntry/index.tsx
+++ src/app/components/DataTable/TableEntry/index.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
-import { inject } from 'mbox-react'
-import { STORE_ROUTER } from 'app/constants'
+// import { inject } from 'mbox-react'
+// import { STORE_ROUTER } from 'app/constants'
 
-@inject(STORE_ROUTER)
+// @inject(STORE_ROUTER)
 
-export default class TableEntry extends React.Component<TableEntryProps, TableEntryState> {
+export default class TableEntry extends React.Component {
   render() {
     return <div>
-      <
+      TableEntry
     </div>
   }
 }
