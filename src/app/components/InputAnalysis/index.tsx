52cf383d79259b0992cba23832e0d690188eb3b3
MichaelGreisberger <mike.greisberger@gmail.com>
2018-12-21 12:52:53 +0100

    added Analysis

---
+++ src/app/components/InputAnalysis/index.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import {STORE_ENTRIES} from "app/constants";
+import {inject, observer} from "mobx-react";
+import * as styles from './InputAnalysis.css' //styles["name"]
+// import { TextInput } from 'react-native';
+
+@inject(STORE_ENTRIES)
+@observer
+export default class InputAnalysis extends React.Component<any> {
+
+    render() {
+        return (
+            <div className={"col-sm-12 " + styles["topContainer"]}>
+                <div className={"col-sm-12"}>
+                    <input className={"col-sm-12 " + styles["fieldBase"]} type={"text"} value={"Titel oder Beschreibung.. "}/>
+                </div>
+                <div className={"col-sm-6"}>
+                    <input className={"col-sm-12 " + styles["fieldBase"]} type={"text"} value={"Max Betrag "}/>
+                    <input className={"col-sm-12 " + styles["fieldBase"]} type={"text"} value={"Min Betrag"}/>
+                    <select className={"col-sm-12 " + styles["fieldBase"]} >
+                        <option value={"Datum absteigend"}>Datum absteigend</option>
+                        <option value={"Datum aufsteigend"}>Datum aufsteigend</option>
+                    </select>
+                </div >
+                <div className="col-sm-6">
+                    <select className={"col-sm-12 " + styles["fieldBase"]} >
+                        <option value={"Alle Benutzer"}>Alle Benutzer</option>
+                        <option value={"Matthias"}>Matthias</option>
+                        <option value={"Markus"}>Markus</option>
+                        <option value={"Gerhard"}>Gerhard</option>
+                        <option value={"Michael"}>Michael</option>
+                    </select>
+                    <select className={"col-sm-12 " + styles["fieldBase"]} >
+                        <option value={"AlleKategorien"}>AlleKategorien</option>
+                        <option value={"Transport"}>Transport</option>
+                        <option value={"Freizeit"}>Freizeit</option>
+                        <option value={"Haushalt"}>Haushalt</option>
+                        <option value={"Essen"}>Essen</option>
+                    </select>
+                    <select className={"col-sm-12 " + styles["fieldBase"]} >
+                        <option value={"Gesamter Zeitraum"}>Gesamter Zeitraum</option>
+                        <option value={"Aktueller Monat"}>Aktueller Monat</option>
+                        <option value={"Zeitraum"}>Zeitraum</option>
+                    </select>
+                </div>
+            </div>
+        )
+    }
+
+
+}
