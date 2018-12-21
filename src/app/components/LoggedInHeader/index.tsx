fe280799fc2bbe4e67b8d848df4a9c8f3c68b027
Gerhard Gruber <gerhard.gruber@documatrix.com>
2018-12-21 16:08:02 +0100

    Worked on header

--- src/app/components/LoggedInHeader/index.tsx
+++ src/app/components/LoggedInHeader/index.tsx
@@ -1,20 +1,69 @@
 import * as React from 'react';
 import { inject } from 'mobx-react';
+import { Link } from 'react-dom-router';
 import {
   STORE_ROUTER,
 } from 'app/constants';
-import { Button } from 'reactstrap';
 
+import { Pie, PieChart, Cell } from 'recharts';
 
 @inject(STORE_ROUTER)
 export default class LoggedInHeader extends React.Component {
   render() {
-    return <div>
-      You are logged in
-      <Button onClick={() => this.props[STORE_ROUTER].logout()}>
-        logout
-      </Button>
-      text text text
+    const monthNames = [
+      "Jänner",
+      "Februar",
+      "März",
+      "April",
+      "Mai",
+      "Juni",
+      "Juli",
+      "August",
+      "September",
+      "Oktober",
+      "November",
+      "Dezember"
+    ];
+
+    const data = [ {
+      name: '',
+      value: 1009.81
+    }, {
+      name: '',
+      value: 3890.19
+    } ]
+
+    return <div className="row" style={{height: "40px"}}>
+      <div className="col-sm-3" style={{position: 'relative', top: '50%', transform: 'translate(0%, -50%)'}}>
+        <Link to="/profile">
+          <span className="fa fa-user" style={{paddingRight: '6px'}} />
+          Richie Rich
+        </Link>
+      </div>
+      <div className="col-sm-6" style={{fontSize: '10px', height: '100%'}}>
+        <table style={{position: 'relative', top: '50%', left: '50%', transform: 'translate(-50%,-50%)'}}>
+          <tr style={{heigth: '50%'}}>
+            <td rowSpan={2}>
+              <PieChart width={30} height={30}>
+                <Pie data={data} isAnimationActive={false} cx={10} cy={10} outerRadius={10} fill="#8884d8">
+                  <Cell fill={"red"} />
+                  <Cell fill={"green"} />
+                </Pie>
+              </PieChart>
+            </td>
+            <td>Aktueller Abrechnungszeitraum: {monthNames[new Date().getMonth()]}</td>
+          </tr>
+          <tr style={{heigth: '50%'}}>
+            <td><b>Ausgegeben: 109,81 €&nbsp;&nbsp;&nbsp;Übrig: 3.890,19 €</b></td>
+          </tr>
+        </table>
+      </div>
+      <div className="col-sm-3" style={{textAlign: 'right', position: 'relative', top: '50%', transform: 'translate(0%, -50%)'}}>
+        <a onClick={() => this.props[STORE_ROUTER].logout()}>
+          Abmelden
+          <span className="fa fa-power-off" style={{paddingLeft: '6px'}} />
+        </a>
+      </div>
     </div>
   }
 }
