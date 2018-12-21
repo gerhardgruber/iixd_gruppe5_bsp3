e735cbcccf71b658e684af725f8bca91f26bca9f
MichaelGreisberger <mike.greisberger@gmail.com>
2018-12-21 16:38:57 +0100

    dashboard wieder als startseite

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 import { hot } from 'react-hot-loader';
 import { Router, Route, Switch } from 'react-router';
 import { Root } from 'app/containers/Root';
-// import { Dashboard } from 'app/containers/Dashboard';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import Analysis from './containers/Analysis';
 import Profile from './containers/Profile';
+import {Dashboard} from "app/containers/Dashboard";
 
 // render react DOM
 export const App = hot(module)(({ history }) => (
@@ -15,7 +15,7 @@ export const App = hot(module)(({ history }) => (
       <Switch>
         <Route path="/analysis" component={Analysis} />
         <Route path="/profile" component={Profile} />
-        <Route exact path="/" component={Analysis} />
+        <Route exact path="/" component={Dashboard} />
       </Switch>
     </Router>
   </Root>
